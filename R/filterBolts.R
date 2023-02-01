cc0fc41e1440c41f97ce0b985b97e1e9aefe7fa1
KateMMiller <kathryn_miller@nps.gov>
2023-02-01 12:58:01 -0500

    updated doc, added filters

--- R/filterBolts.R
+++ R/filterBolts.R
@@ -15,7 +15,7 @@
 #' \describe{
 #' \item{'all'}{Includes all locations returned by other filter arguments in function}
 #' \item{"BASHAR"}{Bass Harbor, ACAD}
-#' \item{"LILTHUN"}{Little Hunter, ACAD}
+#' \item{"LITHUN"}{Little Hunter, ACAD}
 #' \item{"LITMOO"}{Little Moose, ACAD}
 #' \item{"OTTPOI"}{Otter Point, ACAD}
 #' \item{"SCHPOI"}{Schoodic Point, ACAD}
@@ -25,29 +25,16 @@
 #' \item{"OUTBRE"}{Outer Brewster}
 #' }
 #'
-#' @param plotType Type of plot to filter
-#' \describe{
-#' \item{"all"}{Include bolt locations for all plot types}
-#' \item{"band"}{Band transect bolt locations}
-#' \item{"benchmark"}{Benchmark bolt locations}
-#' \item{"photo"}{Photoplot bolt locations}
-#' \item{"pi_transect"}{Bolt locations for point intercept transects}
-#' \item{"recruitment"}{Bolt locations for recruitment plots}
-#' \item{"reference"}{Reference bolt locations}
-#' \item{"temp_logger"}{Bolt locations for temperature loggers}
-#'}
+#' @param plotType Type of plot to filter. Options include:
+#' c("all", "Band Transect", "Benchmark", "Photoplot", "Point Intercept Transect",
+#' "Recruitment Plot", "Reference", "Temperature Logger")
 #'
-#' @param species Filter on target species
-#' \describe{
-#' \item{"all"}{Include bolt locations for all target species}
-#' \item{"ASCO"}{Include only Ascophyllum-related bolt locations}
-#' \item{"BARN"}{Include only Barnacle-related bolt locations}
-#' \item{"ECHI"}{Include only Echinoderm-related bolt locations}
-#' \item{"FUCU"}{Include only Fucus-related bolt locations}
-#' \item{"MUSS"}{Include only Mussel-related bolt locations}
-#' \item{"REDA"}{Include only Red Algae-related bolt locations}
-#' \item{"NOTAPP"}{Include only bolt locations that are not associated with a target species}
-#' }
+#' @param species Filter on target species. Note that "all_records" returns all options, whereas
+#' "All" is part of the Target_Species lookup, and will return records where Target_Species == "All".
+#'  Options include:
+#' c("all_records", "All", "Ascophyllum", "Barnacle", "Echinoderms", "Fucus", "Mussel",
+#' "Red Algae", "Not Applicable")
+#'
 #'
 #' @param plotName Filter on plot name. Options include:
 #'   c("A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "BM",
@@ -60,8 +47,25 @@
 #' @examples
 #' \dontrun{
 #'
-#' #+++++ ADD EXAMPLES +++++
-#' # Make sure they include vectors of locs and plots so obvious you can select multiple
+#' # Default filter returns all records
+#' bolts <- filterBolts()
+#'
+#' # Return photo plot bolts for all sites in BOHA
+#' boha_photo <- filterBolts(park = "BOHA", plotType = "Photoplot")
+#'
+#' # Return bolts for algal only in ACAD's Ship Harbor and Bass Harbor
+#' acad_alg <- filterBolts(location = c("BASHAR", "SHIHAR"),
+#'   species = c("Ascophyllum", "Fucus", "Red Algae"))
+#'
+#' # Return bolts for barnacle recruitment plots in ACAD
+#' acad_barn <- filterBolts(park = "ACAD", )
+#'
+#' # Return bolts for summer barnacle recruitment photoplots at Little Hunter
+#' lihu_sb <- filterBolts(location = "LITHUN", plotName = c("S1", "S2", "S3", "S4", "S5"))
+#'
+#'
+#' # Return bolts for point intercept transects in Outer Brewster
+#' ob_tr <- filterBolts(location = "OUTBRE", plotType = "Point Intercept Transect")
 #'
 #' }
 #'
@@ -69,61 +73,59 @@
 #' @return Returns a data frame of bolt data filtered by function arguments
 #' @export
 
-filterBolts <- function(park = c("all", "ACAD", "BOHA"),
-                        location = c("all","BASHAR", "LILTHUN", "LITMOO", "OTTPOI",
-                                     "SCHPOI", "SHIHAR", "CALISL", "GRISL", "OUTBRE"),
-                        plotType = c("all", "band", "benchmark", "photo", "pi_transect",
-                                     "recruitment", "reference", "temp_logger"),
-                        species = c("all", "ASCO", "BARN", "ECHI", "FUCU", "MUSS", "REDA", "NOTAPP"),
-                        plotName = c("all", "A1", "A2", "A3", "A4", "A5",
-                                     "B1", "B2", "B3", "B4", "B5", "BM",
-                                     "F1", "F2", "F3", "F4", "F5",
-                                     "M1", "M2", "M3", "M4", "M5",
-                                     "R1", "R2", "R3", "R4", "R5", "REF",
-                                     "S1", "S2", "S3", "S4", "S5",
-                                     "T1", "T2", "T3",
-                                     "TEMP1", "TEMP1 ", "TEMP2", "TEMP3",
-                                     "U1", "U2", "U3", "U4", "U5",
-                                     "X1", "X2", "X3")
+filterBolts <- function(park = c("all"),
+                        location = c("all"),
+                        plotType = c("all"),
+                        species = c("all_records"),
+                        plotName = c("all")
                         ){
 
 
-
-  # Match args and class
-  park <- match.arg(park, several.ok = TRUE)
-  location <- match.arg(location)
-  plotType <- match.arg(plotType, several.ok = TRUE)
-  species <- match.arg(species, several.ok = TRUE)
-  plotName <- match.arg(plotName, several.ok = TRUE)
+  # Match args and class; match.args only checks first match in vector, so have to do it more manually.
+  stopifnot(park %in% c("all", "ACAD", "BOHA"))
+  stopifnot(location %in% c("all","BASHAR", "LITHUN", "LITMOO", "OTTPOI",
+                            "SCHPOI", "SHIHAR", "CALISL", "GRISL", "OUTBRE"))
+  stopifnot(plotType %in% c("all", "Band Transect", "Benchmark", "Photoplot", "Point Intercept Transect",
+    "Recruitment Plot", "Reference", "Temperature logger"))
+  stopifnot(species %in% c("all_records", "All", "Ascophyllum", "Barnacle", "Echinoderms", "Fucus",
+                           "Mussel", "Red Algae", "Not Applicable"))
+  stopifnot(plotName %in% c("all", "A1", "A2", "A3", "A4", "A5",
+                            "B1", "B2", "B3", "B4", "B5", "BM",
+                            "F1", "F2", "F3", "F4", "F5",
+                            "M1", "M2", "M3", "M4", "M5",
+                            "R1", "R2", "R3", "R4", "R5", "REF",
+                            "S1", "S2", "S3", "S4", "S5",
+                            "T1", "T2", "T3",
+                            "TEMP1", "TEMP1 ", "TEMP2", "TEMP3",
+                            "U1", "U2", "U3", "U4", "U5",
+                            "X1", "X2", "X3"))
 
   env <- if(exists("ROCKY")){ROCKY} else {.GlobalEnv}
 
   tryCatch(bolts <- get("Bolts", envir = env) |>
-             mutate(ParkCode = ifelse(Site_Name == "Acadia NP", "ACAD", "BOHA")),
+             dplyr::mutate(Park_Code = ifelse(Site_Name == "Acadia NP", "ACAD", "BOHA")),
            error = function(e){stop("Bolts data frame not found. Please import rocky intertidal data.")})
 
+  bolts_park <- if(any(park %in% 'all')){ bolts
+  } else {filter(bolts, Park_Code %in% park)}
 
-  # Fix warning message about only the first element being used.
-  bolts_park <- if(park != 'all'){
-    filter(bolts, ParkCode %in% park)
-  } else {bolts}
+  bolts_loc <- if(any(location %in% 'all')){ bolts_park
+  } else {filter(bolts_park, Loc_Code %in% location)}
 
-  bolts_loc <- if(location != 'all'){
-    filter(bolts_park, Loc_Code %in% location)
-  } else {bolts_park}
+  bolts_plottype <- if(any(plotType %in% 'all')){ bolts_loc
+  } else {filter(bolts_loc, Plot_Type %in% plotType)}
 
-  bolts_plottype <- if(plotType != 'all'){
-    filter(bolts_loc, Plot_Type %in% plotType)
-  } else {bolts_loc}
+  bolts_species <- if(any(species %in% 'all_records')){ bolts_plottype
+  } else {filter(bolts_plottype, Target_Species %in% species)}
 
-  bolts_species <- if(species != 'all'){
-    filter(bolts_plottype, Target_Species %in% species)
-  } else {bolts_plottype}
+  bolts_pname <- if(any(plotName %in% 'all')){ bolts_species
+  } else {filter(bolts_species, Plot_Name %in% plotName)}
 
-  bolts_pname <- if(plotName != 'all'){
-    filter(bolts_species, Plot_Name %in% plotName)
-  } else {bolts_species}
-
+  bolts_final <- bolts_pname |> select(Site_Name, Park_Code, State_Code, Loc_Name, Loc_Code,
+                                       Plot_Name, Plot_Type, Target_Species, Label,
+                                       Bolt_UTM_E, Bolt_UTM_N, Bolt_UTM_Datum,
+                                       Bolt_UTM_Zone, Bolt_NAVD88_Elev, Bolt_MLLW_Elev,
+                                       ListOrder, Notes, Bolt_ID, Plot_ID)
   return(bolts_pname)
 
 }

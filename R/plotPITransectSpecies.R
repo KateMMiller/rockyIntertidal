31a66a1c984828c9e6b159c06348b4d0ad450e27
Kate Miller <kathryn_miller@nps.gov>
2023-05-02 15:32:45 -0400

    completed plotting and doc

--- R/plotPITransectSpecies.R
+++ R/plotPITransectSpecies.R
@@ -4,8 +4,11 @@
 #'
 #' @import ggplot2
 #'
-#' @description This function plots species detections by bolt distance and elevation data by transect for each park,
-#' location, and year.
+#' @description This function plots species by bolt elevation a given park, location, and years.
+#' The point for each species is the median elevation across the three transects for that year.
+#' The thicker lines on the error bars are the 25% and 75% quartiles of elevation across the transects.
+#' The thinner error bars that end with vertical lines are the minimum and maximum elevation detected
+#' along the three transects.
 #'
 #' @param park Include data from all parks, or choose one.
 #' \describe{
@@ -15,6 +18,10 @@
 #' }
 #'
 #' @param location Include data from all locations, or choose specific locations based on location code.
+#' Can only specify one location for each function call.
+#'
+#' @param years Filter on year of data collected. Default is 2013 to current year.
+#'
 #' \describe{
 #' \item{'all'}{Includes all locations returned by other filter arguments in function}
 #' \item{"BASHAR"}{Bass Harbor, ACAD}
@@ -35,10 +42,15 @@
 #' "BOLT", "CHOMAS", "CRUCOR", "FUCEPI", "FUCSPP", "KELP", "MUSSPP", "NONCOR",
 #' "OTHINV", "OTHSUB", "PALPAL", "PORSPP", "ROCK", "ULVENT", "ULVINT", "ULVLAC",
 #' "UNIDEN", "WATER"). If a new species is added, the function will warn the user
-#' that an unrecognized species was specified in case it was an error.
+#' that an unrecognized species was specified in case it was an error. The default
+#' color palette will also need to be updated, if a new species is added. The viridis palette
+#' will work with new species without adaptation, but labels will only be species codes.
 #'
-#' @param years Filter on year of data collected. Default is 2013 to current year.
-#' Can specify a vector of years.
+#' @param palette Choices are "default" or "viridis". Default assigns logical colors to common species.
+#' Viridis uses a color-blind friendly palette of blues, purples and yellows.
+#'
+#' @param facet Logical. If TRUE, will plot species in separate facets. FALSE (default) plots all species
+#' on one figure.
 #'
 #' @param QAQC Logical. If FALSE (Default), does not return QAQC events. If TRUE,
 #' returns all events, including QAQC events.
@@ -50,19 +62,24 @@
 #'
 #' @param ylab Quoted text label for y axis. If not specified, defaults to 'Elevation MLLW (m)'
 #'
+#' @param title If TRUE (Default) prints the full site name on the figure. If FALSE, does not
+#' include plot title.
+#'
 #' @examples
 #' \dontrun{
 #'
 #' importData()
 #'
-#' # Default filter returns all records
-#' plotPITransects()
+#' # Default filter returns a plot for BASHAR (have to pick a site)
+#' plotPITransectSpecies()
 #'
 #' # Other variations
-#' plotPITransects(years = 2018:2021)
-#' plotPITransects(park = "ACAD", years = 2019)
-#' plotPITransects(location = "GREISL", xlab = 'dist', ylab = 'elev')
-#' plotPITransects(location = "BASHAR", plotName = "T1", drop_missing = FALSE)
+#' spp = c("ALGRED", "ASCNOD", "BARSPP", "NONCOR", "FUCSPP", "ULVLAC")
+#' plotPITransectSpecies(location = "CALISL", palette = "default", title = FALSE, facet = TRUE,
+#'                       species = spp)
+#'
+#' plotPITransectSpecies(location = "SHIHAR", palette = "default",
+#'                       facet = FALSE, species = c("BARSPP"))
 #'
 #' }
 #'
@@ -70,9 +87,11 @@
 #' @return Returns a data frame of point intercept species detection data filtered by function arguments
 #' @export
 
-plotPITransectSpecies <- function(park = "all", location = "all", plotName = "all",
-                            species = "all", xlab = "Distance (m)", ylab = "Elevation MLLW (m)",
+plotPITransectSpecies <- function(park = "all", location = "BASHAR", plotName = "all",
+                            species = "all", palette = c('default', 'viridis'),
+                            xlab = "Distance (m)", ylab = "Elevation MLLW (m)",
                             years = 2013:as.numeric(format(Sys.Date(), "%Y")),
+                            facet = FALSE, title = TRUE,
                             QAQC = FALSE, drop_missing = TRUE){
 
 
@@ -80,14 +99,17 @@ plotPITransectSpecies <- function(park = "all", location = "all", plotName = "al
   stopifnot(park %in% c("all", "ACAD", "BOHA"))
   stopifnot(location %in% c("all","BASHAR", "LITHUN", "LITMOO", "OTTPOI",
                             "SCHPOI", "SHIHAR", "CALISL", "GREISL", "OUTBRE"))
-
   stopifnot(plotName %in% c("all", "T1", "T2", "T3"))
   stopifnot(class(years) == "numeric" | class(years) == "integer", years >= 2013)
+  stopifnot(palette %in% c("default", "viridis"))
 
   unmatch_spp <- setdiff(species, c("all", "ALGBRO",  "ALGGRE", "ALGRED", "ARTCOR", "ASCEPI", "ASCNOD", "BARSPP",
                                     "BOLT", "CHOMAS", "CRUCOR", "FUCEPI", "FUCSPP", "KELP", "MUSSPP", "NONCOR",
                                     "OTHINV", "OTHSUB", "PALPAL", "PORSPP", "ROCK", "ULVENT", "ULVINT", "ULVLAC",
                                     "UNIDEN", "WATER"))
+
+  if(length(location) > 1){
+    stop('Must only specify one location or one year, cannot plot multiple locations and years.')}
 
   if(length(unmatch_spp) > 0){
     warning(paste0("Unrecognized species were specified in the species argument: ",
@@ -98,47 +120,83 @@ plotPITransectSpecies <- function(park = "all", location = "all", plotName = "al
 
   stopifnot(exists("ROCKY") | exists("Bolts")) # Checks that ROCKY env exists, or Bolts view is in global env.
 
-  dat <- force(sumPISppDetections(park = park, location = location, plotName = plotName,
+
+
+  # create color palette by species code
+
+  cols = c("ALGBRO" = "#A4755B", "ALGGRE" = "#C4E133", "ALGRED" = "#FF4C53", "ARTCOR" = "#D78AAE",
+           "ASCEPI" = "#85733B", "ASCNOD" = "#C5B47B", "BARSPP" = "#A9A9A9", "CHOMAS" = "#772C27",
+           "CRUCOR" = "#F9F5A1", "NONCOR" = "#9565C9","FUCEPI" = "#D5A82A", "FUCSPP" = "#FFD560",
+           "KELP"   = "#4DA551", "MUSSPP" = "#6F88BF", "OTHINV" = "#F59617", "OTHSUB" = "#000000",
+           "PALPAL" = "#5E5571", "PORSPP" = "#8E3B4A", "ULVENT" = "#699052", "ULVINT" = "#9FCF87",
+           "ULVLAC" = "#73EB31", "UNIDEN" = "#696969", "BOLT"   = "#EAEAEA", "ROCK"   = "#FED5FF",
+           "WATER"  = "#7FC7E1")
+
+  labels = c("ALGBRO" = "Algae - Brown", "ALGGRE" = "Algae - Green", "ALGRED" = "Algae - Red",
+             "ARTCOR" = "Articulated Corallines", "ASCEPI" = "Ascophyllum epibiont",
+             "ASCNOD" = "Ascophyllum nodosum (Knotted wrack)", "CHOMAS" = "Irish moss",
+             "CRUCOR" = "Crustose coraline", "NONCOR" = "Crustose non-coraline",
+             "BARSPP" = "Barnacles",
+             "FUCEPI" = "Fucus epibiont", "FUCSPP" = "Fucus spp. (Rockweed)",
+             "KELP"   = "Kelp", "MUSSPP" = "Muscles",
+             "OTHINV" = "Other invertebrates", "OTHSUB" = "Other substrate",
+             "PALPAL" = "Dulse", "PORSPP" = "Laver", "ULVENT" = "Ulva/Enteromorpha",
+             "ULVINT" = "Ulva intestinalis (Grass kelp)", "ULVLAC" = "Ulva lactuca (Sea lettuce)",
+             "UNIDEN" = "Unidentified", "BOLT"   = "Bolt", "ROCK"   = "Rock", "WATER"  = "Water")
+
+  dat <- suppressWarnings(force(sumPISppDetections(park = park, location = location, plotName = plotName,
                                   years = years, QAQC = QAQC, drop_missing = drop_missing,
-                                  species = species)) |>
-    filter(!is.na(elev_pi))
+                                  species = species))) |>
+    filter(!is.na(PI_Elevation))
+
+  ptitle <- ifelse(title == TRUE, unique(dat$Loc_Name), "")
 
   # Summary stats for species elevations at site level (not transect)
-  dat_sum <- dat |> group_by(Site_Code, Loc_Code, Year, QAQC, Spp_Code) |>
-    summarize(elev_min = min(elev_pi),
-              elev_max = max(elev_pi),
-              elev_med = median(elev_pi),
-              elev_l95 = quantile(elev_pi, probs = 0.025),
-              elev_u95 = quantile(elev_pi, probs = 0.975),
+  dat_sum <- dat |> group_by(Site_Code, Loc_Code, Year, QAQC, Spp_Code, Spp_Name) |>
+    summarize(elev_min = min(PI_Elevation, na.rm = T),
+              elev_max = max(PI_Elevation, na.rm = T),
+              elev_med = median(PI_Elevation, na.rm = T),
+              elev_l95 = quantile(PI_Elevation, probs = 0.025, na.rm = T),
+              elev_u95 = quantile(PI_Elevation, probs = 0.975, na.rm = T),
+              elev_l25 = quantile(PI_Elevation, probs = 0.25, na.rm = T),
+              elev_u75 = quantile(PI_Elevation, probs = 0.75, na.rm = T),
               .groups = 'drop')
 
-  #head(dat_sum)
+  # This is all to make NONCOR species name sort alphabetically with Cs
+  sppcode <- c("ALGBRO", "ALGGRE", "ALGRED", "ARTCOR", "ASCEPI", "ASCNOD",
+               "BARSPP", "CHOMAS", "CRUCOR", "NONCOR", "FUCEPI", "FUCSPP", "KELP",
+               "MUSSPP", "OTHINV", "OTHSUB", "PALPAL", "PORSPP", "ULVENT", "ULVINT",
+               "ULVLAC", "UNIDEN", "BOLT", "ROCK", "WATER")
 
-  #p <-
-    ggplot(dat_sum, aes(x = as.factor(Year), y = elev_med,
-                        group = as.factor(Spp_Code), color = as.factor(Spp_Code))) +
-       geom_point(position = position_dodge(width = 1)) +
-       geom_errorbar(aes(ymin = elev_l95, ymax = elev_u95), position = position_dodge(width = 1)) +
-       scale_y_reverse() +
-       coord_flip() +
-       scale_color_brewer("Species", type = 'div', palette = 'Dark2') +
-       #facet_wrap(~Loc_Code) +
-       theme_rocky() +
-       labs(y = "Elevation MLLW (m)", x = "Year")
+  dat_sum$Spp_Code <- factor(dat_sum$Spp_Code, levels = sppcode) |> droplevels()
+  spp <- levels(dat_sum$Spp_Code)
 
+  cols <- cols[spp]
+  labels <- labels[spp]
 
-       #labs(x = xlab, y = ylab) +
-       # {if(length(unique(dat$Plot_Name)) > 1 & length(unique(dat$Loc_Code)) > 1)
-       #     facet_wrap(~Loc_Code + Plot_Name + Year)} +
-       # {if(length(unique(dat$Plot_Name)) == 1 & length(unique(dat$Loc_Code)) > 1)
-       #     facet_wrap(~Loc_Code + Year)}+
-       # {if(length(unique(dat$Plot_Name)) > 1 & length(unique(dat$Loc_Code)) == 1)
-       #     facet_wrap(~Plot_Name + Year)}+
-       # scale_color_brewer("Year", type = 'div', palette = 'Dark2') +
-       # {if(length(unique(dat$Year)) == 1)
-       #     theme(legend.position = 'none')} +
+  p <-
+  ggplot(dat_sum, aes(x = Year, y = desc(elev_max),
+                        group = Spp_Code, color = Spp_Code, fill = Spp_Code)) +
+         geom_errorbar(aes(ymin = elev_l25, ymax = elev_u75),
+                       position = position_dodge(width = 1), width = 0, linewidth = 1.5) +
+         geom_errorbar(aes(ymin = elev_min, ymax = elev_max),
+                      position = position_dodge(width = 1), linewidth = 0.5) +
+         geom_point(aes(x = Year, y = elev_med),
+                    position = position_dodge(width = 1), size = 3, shape = 21, color = 'black') +
+         {if(all(palette == 'default'))
+           scale_color_manual(values = cols, name = "Species",
+                              breaks = names(cols), labels = labels)} +
+         {if(all(palette == 'default'))
+           scale_fill_manual(values = cols, name = "Species",
+                              breaks = names(cols), labels = labels)} +
+         {if(all(palette == 'viridis')) scale_color_viridis_d("Species")}+
+         {if(facet == TRUE) facet_wrap(~Spp_Code)} +
+         scale_y_reverse(limits = c(max(dat_sum$elev_max), min(dat_sum$elev_min))) +
+         scale_x_reverse(breaks = c(unique(dat_sum$Year)))+
+         coord_flip() +
+         theme_rocky() +
+         labs(y = "Elevation MLLW (m)", x = "Year", title = ptitle)
 
-  p
   return(p)
 
 }

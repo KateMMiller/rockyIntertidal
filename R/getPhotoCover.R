8d1a8ff0c18c1bc7e36a355b1d51271f5a4e930c
KateMMiller <kathryn_miller@nps.gov>
2024-09-05 14:37:15 -0400

    updated for new DP

--- R/getPhotoCover.R
+++ R/getPhotoCover.R
@@ -5,7 +5,7 @@
 #' @importFrom dplyr filter left_join mutate select
 #'
 #' @description This function filters photo quadrat percent cover data by park,
-#' location, plot name, and species.
+#' site, plot name, and species.
 #'
 #' @param park Include data from all parks, or choose one.
 #' \describe{
@@ -14,9 +14,9 @@
 #' \item{'BOHA'}{Includes only sites in Boston Harbor Islands National Recreation Area}
 #' }
 #'
-#' @param location Include data from all locations, or choose specific locations based on location code.
+#' @param site Include data from all sites, or choose specific sites based on site code.
 #' \describe{
-#' \item{'all'}{Includes all locations returned by other filter arguments in function}
+#' \item{'all'}{Includes all sites returned by other filter arguments in function}
 #' \item{"BASHAR"}{Bass Harbor, ACAD}
 #' \item{"LITHUN"}{Little Hunter, ACAD}
 #' \item{"LITMOO"}{Little Moose, ACAD}
@@ -25,7 +25,7 @@
 #' \item{"SHIHAR"}{Ship Harbor, ACAD}
 #' \item{"CALISL"}{Calf Island, BOHA}
 #' \item{"GREISL"}{Green Island, BOHA}
-#' \item{"OUTBRE"}{Outer Brewster}
+#' \item{"OUTBRE"}{Outer Brewster, BOHA}
 #' }
 #'
 #' @param plotName Filter on plot name. Options include:
@@ -43,7 +43,7 @@
 #' @param category Filter on category. Options include:
 #' c("all", "Genus", "Species", "Species Group", and "Substrate")
 #'
-#' @param target_species Filter on target species (ie photoplot). Options include:
+#' @param community Filter on target community. Options include:
 #' c("Ascophyllum", "Barnacle", "Fucus", "Mussel", "Red Algae")
 #'
 #' @param years Filter on year of data collected. Default is 2013 to current year.
@@ -66,7 +66,7 @@
 #' # Species detections for specific sites, plots, species, and years
 #'
 #' cov_t3 <- getPhotoCover(park = "ACAD", plotName = "T3")
-#' cov_BOHA2 <- getPhotoCover(location = c("CALISL", "GREISL"))
+#' cov_BOHA2 <- getPhotoCover(site = c("CALISL", "GREISL"))
 #' cov_ab <- getPhotoCover(park = "BOHA", species = c("ALGBRO"))
 #' cov_5yr <- getPhotoCover(years = 2016:2021)
 #' cov_first_last <- getPhotoCover(years = c(2013, 2021))
@@ -78,16 +78,17 @@
 #' @return Returns a data frame of photo plot percent cover data filtered by function arguments
 #' @export
 
-getPhotoCover <- function(park = "all", location = "all", plotName = "all",
-                          species = "all", category = "all", target_species = 'all',
+getPhotoCover <- function(park = "all", site = "all", plotName = "all",
+                          species = "all", #category = "all",
+                          community = 'all',
                           years = 2013:as.numeric(format(Sys.Date(), "%Y")), QAQC = FALSE){
 
 
   # Match args and class; match.args only checks first match in vector, so have to do it more manually.
   stopifnot(park %in% c("all", "ACAD", "BOHA"))
-  stopifnot(location %in% c("all","BASHAR", "LITHUN", "LITMOO", "OTTPOI",
+  stopifnot(site %in% c("all","BASHAR", "LITHUN", "LITMOO", "OTTPOI",
                             "SCHPOI", "SHIHAR", "CALISL", "GREISL", "OUTBRE"))
-  stopifnot(target_species %in% c("all", "Ascophyllum", "Barnacle", "Fucus", "Mussel", "Red Algae"))
+  stopifnot(community %in% c("all", "Ascophyllum", "Barnacle", "Fucus", "Mussel", "Red Algae"))
   unmatch_spp <- setdiff(species, c("all", "ALGBRO",  "ALGGRE", "ALGRED", "ARTCOR", "ASCEPI", "ASCNOD", "BARSPP",
                                     "CHOMAS", "CRUCOR", "FUCEPI", "FUCSPP", "KELP", "MUSSPP", "NONCOR", "NOSAMP",
                                     "OTHINV", "OTHPLA", "OTHSUB", "PALPAL", "PORSPP", "ROCK", "SAND", "TAR",
@@ -100,7 +101,7 @@ getPhotoCover <- function(park = "all", location = "all", plotName = "all",
                    "Check that this wasn't a typo."))
   }
 
-  stopifnot(category %in% c("all", "Genus", "Species", "Species Group", "Substrate"))
+  #stopifnot(category %in% c("all", "Genus", "Species", "Species Group", "Substrate"))
   stopifnot(plotName %in% c("all", "A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5",
                             "F1", "F2", "F3", "F4", "F5", "M1", "M2", "M3", "M4", "M5",
                             "R1", "R2", "R3", "R4", "R5"))
@@ -109,51 +110,48 @@ getPhotoCover <- function(park = "all", location = "all", plotName = "all",
   env <- if(exists("ROCKY")){ROCKY} else {.GlobalEnv}
 
   tryCatch(cover <- get("PhotoQuadrats_Cover", envir = env) |>
-             dplyr::mutate(Year = as.numeric(format(Start_Date, "%Y"))),
+             dplyr::mutate(Year = as.numeric(format(StartDate, "%Y"))),
            error = function(e){stop("PhotoQuadrats_Cover data frame not found. Please import rocky intertidal data.")})
 
-  bolts1 <- force(getBolts(park = park, location = location, plotName = plotName,
-                          target_species = 'all_records', plotType = 'Photoplot')) |>
+  bolts1 <- force(getBolts(park = park, site = site, plotName = plotName,
+                          community = 'all_records', plotType = 'Photoplot')) |>
     filter(grepl("label", Label))
 
-  bolts <- if(any(target_species %in% 'all')){bolts1
-    } else {filter(bolts1, Target_Species %in% target_species)}
+  bolts <- if(any(community %in% 'all')){bolts1
+    } else {filter(bolts1, CommunityType %in% community)}
 
-  cov_park <- if(any(park %in% 'all')){ filter(cover, Site_Code %in% c("ACAD", "BOHA"))
-  } else {filter(cover, Site_Code %in% park)}
+  cov_park <- if(any(park %in% 'all')){ cover
+  } else {filter(cover, UnitCode %in% park)}
 
-  cov_loc <- if(any(location %in% 'all')){ cov_park
-  } else {filter(cov_park, Loc_Code %in% location)}
+  cov_loc <- if(any(site %in% 'all')){ cov_park
+  } else {filter(cov_park, SiteCode %in% site)}
 
   cov_pname <- if(any(plotName %in% 'all')){ cov_loc
-  } else {filter(cov_loc, Plot_Name %in% plotName)}
+  } else {filter(cov_loc, PlotName %in% plotName)}
 
   cov_species <- if(any(species %in% 'all')){ cov_pname
-  } else {filter(cov_pname, Spp_Code %in% species)}
+  } else {filter(cov_pname, SpeciesCode %in% species)}
 
-  cov_targ <- if(any(target_species %in% "all")){cov_species
-  } else{filter(cov_species, Target_Species %in% target_species)}
+  cov_targ <- if(any(community %in% "all")){cov_species
+  } else{filter(cov_species, CommunityType %in% community)}
 
-  cov_cat <- if(any(category %in% 'all')){ cov_targ
-  } else {filter(cov_targ, Category %in% category)}
+  # cov_cat <- if(any(category %in% 'all')){ cov_targ
+  # } else {filter(cov_targ, Category %in% category)}
 
-  cov_year <- filter(cov_cat, Year %in% years)
+  cov_year <- filter(cov_targ, Year %in% years)
 
-  cov_qaqc <- if(QAQC == TRUE){cov_year |>
-      select(Site_Name, Site_Code, Loc_Name, Loc_Code, Start_Date, Year, Date_Scored,
-             QAQC, Plot_Name, Target_Species, Spp_Code, Spp_Name, Category, Perc_Cover,
-             Notes, Scorer, Event_ID, Plot_ID, QAQC_SameGrid, QAQC_NewGrid)
-  } else {cov_year |> filter(QAQC == FALSE) |>
-      select(Site_Name, Site_Code, Loc_Name, Loc_Code, Start_Date, Year,
-             Date_Scored, QAQC, Plot_Name,
-             Target_Species, Spp_Code, Spp_Name, Category, Perc_Cover, Notes,
-             Scorer, Event_ID, Plot_ID)}
+  cov_qaqc <- if(QAQC == TRUE){cov_year
+  } else {cov_year |> filter(QAQC == FALSE) }
 
   # Join bolt elevation with cover data
-  cov_comb <- left_join(bolts |> select(Site_Code, Loc_Code, Plot_Name, Target_Species,
-                                        Bolt_UTM_E, Bolt_UTM_N, Bolt_MLLW_Elev) |> unique(),
+  cov_comb <- left_join(bolts |> select(UnitCode, SiteCode, PlotName, CommunityType,
+                                        BoltLatitude, BoltLongitude, Bolt_UTM_E, Bolt_UTM_N, Bolt_MLLW_Elev) |> unique(),
                     cov_qaqc,
-                    by = c("Site_Code", "Loc_Code", "Plot_Name", "Target_Species"))
+                    by = c("UnitCode", "SiteCode", "PlotName", "CommunityType")) |>
+
+  select(GroupCode, GroupName, UnitCode, UnitName, SiteCode, SiteName, StartDate, Year, QAQC,
+         PlotName, CommunityType, BoltLatitude, BoltLongitude, Bolt_UTM_E, Bolt_UTM_N, Bolt_MLLW_Elev,
+         CoverType, ScientificName, SpeciesCode, PercentCover, IsPointCUI)
 
   if(nrow(cov_comb) == 0){stop("Specified arguments returned an empty data frame.")}
 
